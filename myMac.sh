73cb01d05d76626f4e131dee057f46679433473d
simmontali <sim.montali@gmail.com>
2021-02-11 19:55:33 +0100

    Added my configuration for macs

---
+++ myMac.sh
@@ -0,0 +1,29 @@
+echo "I'll need your password probably. Let's solve this at the root.✨"
+
+sudo -V
+
+brew install zsh-syntax-highlighting mongodb python3 wget tracert ImageMagick fortune cowsay libnfc automake pk
+~                                                                                                                      │g-config texlive mactex-no-gui skim graphviz httrack weechat openssl telnet nmap httpie iperf cdrtools bat hub tree py
+~                                                                                                                      │gments autojump tmux ipython
+
+brew install zsh-syntax-highlighting python3 wget ImageMagick fortune cowsay libnfc automake pkg-config mactex-
+~                                                                                                                      │no-gui graphviz httrack weechat openssl telnet nmap httpie iperf cdrtools bat hub tree pygments autojump tmux ipython
+
+gem install lolcat
+
+brew install gist
+brew install motd
+pip3 install rebound-cli
+
+brew install --cask background-music
+
+% Conda Install
+cd ~/Downloads
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+bash miniconda.sh -b -p $HOME/miniconda
+echo export PATH="~/miniconda/bin:$PATH" > ~/.zshrc
+cd ~/miniconda/bin && conda update conda
+conda init zsh
+
+pip3 install jupyter jupyter-client jupyter-console jupyter-core numpy pandas
+
